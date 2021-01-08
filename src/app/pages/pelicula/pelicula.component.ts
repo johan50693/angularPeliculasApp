ba1c19afcb96646cf9ee6834886d2cc663eb4782
Albert <johan50693@gmail.com>
2021-01-08 15:58:54 -0400

    Se agrega ajuste para integrar multiples llamadas al servicio en un solo elemento

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest } from 'rxjs';
 import { Cast } from 'src/app/interfaces/credits-response';
 import { MovieDetail } from 'src/app/interfaces/movie-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
@@ -22,19 +23,33 @@ export class PeliculaComponent implements OnInit {
   ngOnInit() {
     const id= this.route.snapshot.params.id;
     // console.log(id);
-    this.peliculasservice.getPeliculaDetalle(id).subscribe(resp =>{
-      // console.log(resp);
-      if(!resp){
+
+    combineLatest([
+      this.peliculasservice.getPeliculaDetalle(id),
+      this.peliculasservice.getCast(id)
+    ]).subscribe( ([movie,cast]) =>{
+      if(!movie){
         this.router.navigateByUrl('/home');
         return;
       }
-      this.pelicula= resp;
+
+      this.pelicula= movie;
+      this.cast=cast.filter( actor => {return actor.profile_path != null});
     });
 
-    this.peliculasservice.getCast(id).subscribe(resp => {
-      this.cast=resp.filter( actor => {return actor.profile_path != null});
-      // console.log(resp);
-    });
+    // this.peliculasservice.getPeliculaDetalle(id).subscribe(resp =>{
+    //   // console.log(resp);
+    //   if(!resp){
+    //     this.router.navigateByUrl('/home');
+    //     return;
+    //   }
+    //   this.pelicula= resp;
+    // });
+
+    // this.peliculasservice.getCast(id).subscribe(resp => {
+    //   this.cast=resp.filter( actor => {return actor.profile_path != null});
+    //   // console.log(resp);
+    // });
   }
 
   onRegresar(){
